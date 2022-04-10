dd2d6c1c20c9f041b393732db6c2d8292b2e5960
Martin1301 <67760752+Martin1301@users.noreply.github.com>
2022-04-10 12:26:24 -0500

    Agregar archivos de proyecto.

---
+++ DemoLayout2022/DemoLayout2022/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoLayout2022
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            //Primera página que se mostrará
+            MainPage = new StackLayoutDemo();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ DemoLayout2022/DemoLayout2022/Page5.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoLayout2022
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page5 : ContentPage
+    {
+        public Page5()
+        {
+            StackLayout stack = new StackLayout();
+            Button button = new Button();
+            button.Text = "Ejemplo";
+
+            Label label = new Label();
+            label.Text = "Ejeplo";
+
+            stack.Children.Add(button);
+            stack.Children.Add(label);
+
+            this.Content = stack;
+
+        }
+    }
+}
